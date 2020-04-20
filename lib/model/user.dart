a51c640f735430d02ee7821b7c5343e7947842a0
evandrobss <evandro.bss@outlook.com>
2020-04-20 13:25:41 -0300

    Alteraçao no arquivo user.dart

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,7 +1,6 @@
-class User{
-
+class User {
   //variaveis
-  Int user_id;
+  int user_id;
   String user_login;
   String user_password;
   String user_display_name;
@@ -9,20 +8,26 @@ class User{
   Float user_long;
 
   // método construtor da classe
-  User({this.user_id,this.user_login,this.user_password,this.user_display_name,this.user_lat, this.user_long});
+  User(
+      {this.user_id,
+      this.user_login,
+      this.user_password,
+      this.user_display_name,
+      this.user_lat,
+      this.user_long});
 
   // Método que pega os dados do arquivo Json
-  User.fromJson(Map<String, dynamic> json){
-    this.user_id = json['id']
-    this.userlogin = json['user_login'];
+  User.fromJson(Map<String, dynamic> json) {
+    this.user_id = json['id'];
+    this.user_login = json['user_login'];
     this.user_password = json['user_password'];
     this.user_display_name = json['user_display_name'];
     this.user_lat = json['user_lat'];
     this.user_long = json['user_long'];
   }
-  
+
   //Método que constroi o arquivo Json
-  Map<String, dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['user_id'] = this.user_id;
     data['user_login'] = this.user_login;
@@ -32,5 +37,4 @@ class User{
     data['user_long'] = this.user_long;
     return data;
   }
-
 }
